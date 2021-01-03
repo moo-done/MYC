f4e8daa4d8c36e942a794429f309cd8c59dc046b
wonseon <ws.cho@m2cloud.kr>
2021-01-03 03:24:55 -0800

    인스타그램 사진 게시 컴포넌트

--- src/app/page/intro/instagram-photo/instagram-photo.component.ts
+++ src/app/page/intro/instagram-photo/instagram-photo.component.ts
@@ -12,4 +12,22 @@ export class InstagramPhotoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  readonly instagram_imgs1 : string[] =
+    [
+      'assets/introImg/instargram_img/IMG_6291%202.JPG',
+      'assets/introImg/instargram_img/IMG_6282%202.JPG',
+      'assets/introImg/instargram_img/IMG_6283%202.JPG',
+      'assets/introImg/instargram_img/IMG_6284%202.JPG',
+      'assets/introImg/instargram_img/IMG_6286%202.JPG',
+    ];
+
+
+  readonly instagram_imgs2 : string[] =
+    [
+      'assets/introImg/instargram_img/IMG_6294%202.JPG',
+      'assets/introImg/instargram_img/IMG_6297%202.JPG',
+      'assets/introImg/instargram_img/IMG_6298%202.JPG',
+      'assets/introImg/instargram_img/IMG_6299%202.JPG',
+      'assets/introImg/instargram_img/IMG_6300%202.JPG',
+    ];
 }
